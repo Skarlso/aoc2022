164334e0796de377dd2e0e652023326df7fe808e
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2022-12-28 17:21:30 +0100

    I was missing the initial coordinates.

--- day18/part1/main.go
+++ day18/part1/main.go
@@ -24,6 +24,16 @@ func (p point) String() string {
 	return fmt.Sprintf("%d%d%d", p.x, p.y, p.z)
 }
 
+// sides
+var sides = []point{
+	{x: 0, y: 0, z: 1},
+	{x: 0, y: 1, z: 0},
+	{x: 1, y: 0, z: 0},
+	{x: 0, y: 0, z: -1},
+	{x: 0, y: -1, z: 0},
+	{x: -1, y: 0, z: 0},
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: go run part1/main.go [file]")
@@ -33,7 +43,9 @@ func main() {
 	content, _ := os.ReadFile(file)
 	split := strings.Split(string(content), "\n")
 
-	globalSides := make(map[string]int)
+	globalSides := make(map[point]bool)
+
+	listOfPoints := make([]point, 0)
 	for _, line := range split {
 		if line == "" {
 			continue
@@ -42,36 +54,20 @@ func main() {
 			x, y, z int
 		)
 		fmt.Sscanf(line, "%d,%d,%d", &x, &y, &z)
-
-		// up
-		up := fmt.Sprintf("%s%s%s%s", point{x: x, y: y + 1, z: z}, point{x: x, y: y + 1, z: z + 1}, point{x: x + 1, y: y + 1, z: z + 1}, point{x: x + 1, y: y + 1, z: z})
-		globalSides[up]++
-		// down
-		down := fmt.Sprintf("%s%s%s%s", point{x: x, y: y, z: z}, point{x: x, y: y, z: z + 1}, point{x: x + 1, y: y, z: z + 1}, point{x: x + 1, y: y, z: z})
-		globalSides[down]++
-		// left
-		left := fmt.Sprintf("%s%s%s%s", point{x: x, y: y, z: z}, point{x: x, y: y + 1, z: z}, point{x: x, y: y + 1, z: z + 1}, point{x: x, y: y, z: z + 1})
-		globalSides[left]++
-		// right
-		right := fmt.Sprintf("%s%s%s%s", point{x: x + 1, y: y, z: z}, point{x: x + 1, y: y + 1, z: z}, point{x: x + 1, y: y + 1, z: z + 1}, point{x: x + 1, y: y, z: z + 1})
-		globalSides[right]++
-		// front
-		front := fmt.Sprintf("%s%s%s%s", point{x: x, y: y, z: z}, point{x: x, y: y + 1, z: z}, point{x: x + 1, y: y, z: z}, point{x: x + 1, y: y + 1, z: z})
-		globalSides[front]++
-		// back
-		back := fmt.Sprintf("%s%s%s%s", point{x: x, y: y, z: z + 1}, point{x: x, y: y + 1, z: z + 1}, point{x: x + 1, y: y, z: z + 1}, point{x: x + 1, y: y + 1, z: z + 1})
-		globalSides[back]++
-
-		//hash := fmt.Sprintf("%s-%s-%s-%s-%s-%s", up, down, left, right, front, back)
-		// Create a hash of all sides and put them into a global map[string]int.
-		// Anything with 1 is a side that doesn't have a pair.
-		//globalSides[hash]++
+		p := point{x: x, y: y, z: z}
+		listOfPoints = append(listOfPoints, p)
+		globalSides[p] = true
 	}
-	loneSides := 0
-	for _, v := range globalSides {
-		if v == 1 {
-			loneSides++
+
+	totalLoneSides := 0
+	for _, p := range listOfPoints {
+		loneSides := 6
+		for _, s := range sides {
+			if globalSides[point{x: p.x + s.x, y: p.y + s.y, z: p.z + s.z}] {
+				loneSides--
+			}
 		}
+		totalLoneSides += loneSides
 	}
-	fmt.Println("lone sides: ", loneSides)
+	fmt.Println("lone sides: ", totalLoneSides)
 }
