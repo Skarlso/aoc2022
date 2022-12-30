8d59e602f05362d91e5b521691efb1637e6d423a
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2022-12-30 10:11:36 +0100

    Almost there

--- day17/part1/main.go
+++ day17/part1/main.go
@@ -9,7 +9,8 @@ import (
 // We will be counting upwards as `+y` instead of a grid. We will handle the grid as a Cartesian coordinate system.
 var (
 	l = shape{
-		top: point{x: 2, y: 2},
+		height: 3,
+		top:    point{x: 2, y: 2},
 		leftSide: []point{
 			{x: 0, y: 0},
 			{x: 2, y: 1},
@@ -37,6 +38,7 @@ var (
 		name: "L",
 	}
 	minus = shape{
+		height:    1,
 		name:      "-",
 		top:       point{x: 0, y: 0},
 		mostRight: point{x: 3, y: 0},
@@ -61,6 +63,7 @@ var (
 		},
 	}
 	block = shape{
+		height:    2,
 		name:      "B",
 		top:       point{x: 0, y: 1},
 		mostRight: point{x: 1, y: 0},
@@ -85,6 +88,7 @@ var (
 		},
 	}
 	plus = shape{
+		height:    3,
 		name:      "+",
 		top:       point{x: 1, y: 1},
 		mostRight: point{x: 2, y: 0},
@@ -113,6 +117,7 @@ var (
 		},
 	}
 	linePiece = shape{
+		height:    4,
 		name:      "|",
 		top:       point{x: 0, y: 3},
 		mostRight: point{x: 0, y: 0},
@@ -163,6 +168,7 @@ var (
 // ##
 // ##
 type shape struct {
+	height     int
 	leftSide   []point
 	rightSide  []point
 	top        point
@@ -198,44 +204,47 @@ func main() {
 	// fmt.Println(fallingOrder)
 	fmt.Println("jet pattern: ", jetPattern)
 
-	y := 4
-	unitsOfTall := 0
-	// endingPoint := 0
-
-	rocks := 2
+	y := 0
+	rocks := 1
 	fallen := 0
 	playground := make(map[point]bool)
 	leftSide := 0
 	rightSide := 7
 	jetPush := 0
-	display(playground, y)
 	for fallen != rocks {
+		// starting position is the highest point + 3
+		startingY := y + 3
 		currentRock := fallingOrder[fallen%len(fallingOrder)]
 		// fmt.Println("current falling rock: ", currentRock.name)
 		// make sure we are 3 above the last fallen rock's last y coordinate considering our current falling rock's
 		// lowest point.
 		y += currentRock.bottom.y
-		start := point{x: 2, y: y} // This is the location of the designated zeroth coordinate of the shape.
-		// lowestPoint := lowest(rockCoordinate, currentRock)
-		// // Remember that this is +y because we are in a Cartesian coordinate system, I decided.
-		// highestPoint := highest(rockCoordinate, currentRock)
+		current := point{x: 2, y: startingY} // This is the location of the designated zeroth coordinate of the shape.
 
-		queue := []point{start}
-		var current point
-		for len(queue) > 0 {
-			fmt.Println("moving")
-			display(playground, y)
+		// fmt.Println("starting point: ", current)
+		// fmt.Println("current rock: ", currentRock.name)
+		// display(playground, y, current, currentRock)
+		// queue := []point{start}
+		// goes until it can no longer move.
+		canMove := true
+		for canMove {
+			display(playground, y, current, currentRock)
+			canMove = false
 			// Determine the coordinate of each piece of the shape as it falls down.
 			// let it fall until any of its coordinates hits anything
-			current, queue = queue[0], queue[1:]
-
+			// current, queue = queue[0], queue[1:]
+			// fmt.Printf("current: %+v\n", current)
 			// move down
 			// if we didn't reach the end and we didn't hit anything on the way down,
 			// we increase the x, y coordinate which will track our rock across the movement.
-			if (current.y-1+currentRock.bottom.y) > 0 && !isSomethingDownwards(current, currentRock, playground) {
+			// fmt.Println("current.y-1+currentRock.bottom.y: ", current.y-1+currentRock.bottom.y)
+			if (current.y-1+currentRock.bottom.y) >= 0 && !isSomethingDownwards(current, currentRock, playground) {
+				// fmt.Println("y--")
 				// add it to the new round, and then push the block.
-				queue = append(queue, point{x: current.x, y: current.y - 1})
+				// queue = append(queue, point{x: current.x, y: current.y - 1})
 				current.y--
+				canMove = true
+				// fmt.Println("y: ", current.y)
 				// if we hit anything, the push still happens, but it might just not move it. however, the push
 				// might have pushed it into a direction that now lets it fall.
 				// so once we push we check again if anything is below us. If not, we include this check again.
@@ -244,6 +253,7 @@ func main() {
 			// push the block
 
 			currentJet := jetPattern[jetPush%len(jetPattern)]
+			// fmt.Println("jet: ", currentJet)
 			if currentJet == "<" {
 				// The x is at the left side, so this is okay.
 				// But I must also consider all points because it could blow the rock into a crevasse.
@@ -251,50 +261,64 @@ func main() {
 					current.x--
 				}
 			} else if currentJet == ">" {
-				if current.x+currentRock.mostRight.x < rightSide && !isSomethingToTheRight(current, currentRock, playground) {
+				if current.x+1+currentRock.mostRight.x < rightSide && !isSomethingToTheRight(current, currentRock, playground) {
 					current.x++
 				}
 			}
 			jetPush++
 
-			// check again if something is below us, if not, we add it to the queue, but we don't decrease y again
+			// check if something is below is. If yes, we set canMove to true so in the next round it will move.
 			if (current.y-1+currentRock.bottom.y) > 0 && !isSomethingDownwards(current, currentRock, playground) {
 				// add it to the new round, and then push the block.
-				queue = append(queue, point{x: current.x, y: current.y})
+				canMove = true
+				// current.y--
 			}
+			display(playground, y, current, currentRock)
 		}
 
 		// Once the rock stopped moving we add each point of it to the playground.
 		for _, p := range allPoints(current, currentRock) {
+			// fmt.Println("This is never getting called?")
 			playground[p] = true
 		}
 
-		// shit, I need to increase TALL
-		// Maybe this is fine.
-		// increase the y at which we start.
-		if current.y+currentRock.top.y > y {
-			y += current.y + currentRock.top.y
+		// The highest point is the current y + height.
+		if current.y+currentRock.height > y {
+			y += current.y + currentRock.height
 			// I have to re-think this one.
-			unitsOfTall += (current.y + currentRock.top.y - unitsOfTall)
+			// unitsOfTall += (current.y + currentRock.top.y - unitsOfTall)
 		}
 
 		fallen++
+		display(playground, y, current, currentRock)
 	}
 
-	fmt.Println("The tower is this tall: ", unitsOfTall)
+	fmt.Println("The tower is this tall: ", y)
 }
 
-func display(playground map[point]bool, maxy int) {
-	for y := 0; y < maxy; y++ {
+func display(playground map[point]bool, maxy int, current point, rock shape) {
+	contains := func(p point, current point, list []point) bool {
+		for _, v := range list {
+			if p.x == v.x+current.x && p.y == v.y+current.y {
+				return true
+			}
+		}
+
+		return false
+	}
+	for y := maxy + 7; y >= 0; y-- {
 		for x := 0; x < 7; x++ {
 			if playground[point{x: x, y: y}] {
 				fmt.Print("#")
+			} else if contains(point{x: x, y: y}, current, rock.all) {
+				fmt.Print("@")
 			} else {
 				fmt.Print(".")
 			}
 		}
 		fmt.Println()
 	}
+	fmt.Println("-------")
 }
 
 func allPoints(current point, rock shape) []point {
