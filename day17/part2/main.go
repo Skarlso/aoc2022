11b69d79169fb2f401574f1aebf216371c91d54c
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2022-12-30 19:52:51 +0100

    Left hints whenever I feel like coding this thing up

--- day17/part2/main.go
+++ day17/part2/main.go
@@ -2,21 +2,214 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"os"
 	"strings"
 )
 
-type sensor struct {
-	p        point
-	coverage int
-	beacon   point
+// We will be counting upwards as `+y` instead of a grid. We will handle the grid as a Cartesian coordinate system.
+var (
+	l = shape{
+		height:   3,
+		top:      point{x: 2, y: 2},
+		mostLeft: point{x: 0, y: 0},
+		leftSide: []point{
+			{x: 0, y: 0},
+			{x: 1, y: 0},
+			{x: 2, y: 1},
+			{x: 2, y: 2},
+		},
+		rightSide: []point{
+			{x: 1, y: 0},
+			{x: 2, y: 0},
+			{x: 2, y: 1},
+			{x: 2, y: 2},
+		},
+		bottomSide: []point{
+			{x: 0, y: 0},
+			{x: 1, y: 0},
+			{x: 2, y: 0},
+		},
+		mostRight: point{x: 2, y: 0},
+		bottom:    point{x: 0, y: 0},
+		all: []point{
+			{x: 0, y: 0},
+			{x: 1, y: 0},
+			{x: 2, y: 0},
+			{x: 2, y: 1},
+			{x: 2, y: 2},
+		},
+		name: "L",
+	}
+	minus = shape{
+		height:    1,
+		name:      "-",
+		top:       point{x: 0, y: 0},
+		mostRight: point{x: 3, y: 0},
+		bottom:    point{x: 0, y: 0},
+		mostLeft:  point{x: 0, y: 0},
+		leftSide: []point{
+			{x: 0, y: 0},
+		},
+		rightSide: []point{
+			{x: 3, y: 0},
+		},
+		bottomSide: []point{
+			{x: 0, y: 0},
+			{x: 1, y: 0},
+			{x: 2, y: 0},
+			{x: 3, y: 0},
+		},
+		all: []point{
+			{x: 0, y: 0},
+			{x: 1, y: 0},
+			{x: 2, y: 0},
+			{x: 3, y: 0},
+		},
+	}
+	block = shape{
+		height:    2,
+		name:      "B",
+		top:       point{x: 0, y: 1},
+		mostRight: point{x: 1, y: 0},
+		bottom:    point{x: 0, y: 0},
+		mostLeft:  point{x: 0, y: 0},
+		leftSide: []point{
+			{x: 0, y: 0},
+			{x: 0, y: 1},
+		},
+		rightSide: []point{
+			{x: 1, y: 0},
+			{x: 1, y: 1},
+		},
+		bottomSide: []point{
+			{x: 0, y: 0},
+			{x: 1, y: 0},
+		},
+		all: []point{
+			{x: 0, y: 0},
+			{x: 1, y: 0},
+			{x: 0, y: 1},
+			{x: 1, y: 1},
+		},
+	}
+	plus = shape{
+		height:    3,
+		name:      "+",
+		top:       point{x: 0, y: 2},
+		mostRight: point{x: 1, y: 1},
+		mostLeft:  point{x: -1, y: 1},
+		bottom:    point{x: 0, y: 0},
+		leftSide: []point{
+			{x: -1, y: 1},
+			{x: 0, y: 0},
+			{x: 0, y: 2},
+		},
+		rightSide: []point{
+			{x: 0, y: 0},
+			{x: 1, y: 1},
+			{x: 0, y: 2},
+		},
+		bottomSide: []point{
+			{x: -1, y: 1},
+			{x: 0, y: 0},
+			{x: 1, y: 1},
+		},
+		all: []point{
+			{x: 0, y: 0},
+			{x: -1, y: 1},
+			{x: 0, y: 2},
+			{x: 1, y: 1},
+			{x: 0, y: 1},
+		},
+	}
+	linePiece = shape{
+		height:    4,
+		name:      "|",
+		top:       point{x: 0, y: 3},
+		mostRight: point{x: 0, y: 0},
+		bottom:    point{x: 0, y: 0},
+		mostLeft:  point{x: 0, y: 0},
+		leftSide: []point{
+			{x: 0, y: 0},
+			{x: 0, y: 1},
+			{x: 0, y: 2},
+			{x: 0, y: 3},
+		},
+		rightSide: []point{
+			{x: 0, y: 0},
+			{x: 0, y: 1},
+			{x: 0, y: 2},
+			{x: 0, y: 3},
+		},
+		bottomSide: []point{
+			{x: 0, y: 0},
+		},
+		all: []point{
+			{x: 0, y: 0},
+			{x: 0, y: 1},
+			{x: 0, y: 2},
+			{x: 0, y: 3},
+		},
+	}
+)
+
+// Share is made up of a set of points that we will add to the current grid.
+// They will be used to continuously calculate where each point of a shape is
+// located at at any given moment in the movement.
+// Falling order:
+// ####
+//
+// .#.
+// ###
+// .#.
+//
+// ..#
+// ..#
+// ###
+//
+// #
+// #
+// #
+// #
+//
+// ##
+// ##
+type shape struct {
+	height     int
+	leftSide   []point
+	rightSide  []point
+	top        point
+	mostRight  point
+	mostLeft   point
+	bottom     point
+	bottomSide []point
+	all        []point
+
+	name string
 }
 
 type point struct {
 	x, y int
 }
 
+// FUCK PART 2. :D
+
+// Store map in a dictionary of sets representing each column. Each set contains the y coordinates for that column with a rock in it.
+
+// For cycle detection, I:
+//     Created a "state" tuple each time a rock came to rest
+//     Said tuple included the relative position of the highest y coordinate in each column (so the lowest y = 0 and the others are how many positions higher than the lowest)
+//     The tuple also included the rock type and the jet stream index
+//     Added the tuple to a "states" dictionary, where the tuple is the key, and the value was another dictionary with keys "height" and "rock" -- indicating the height at this state and how many rocks have fallen
+
+// When a repeated cycle is detected, I:
+
+// calculate how many rocks fell and height was gained during the cycle
+// calculate the remaining number of cycles
+// calculate the height that will be gained in the remaining cycles and store it for later
+// calculate the remaining rocks after we complete cycles
+// set the current rock_count to the total minus the remainder
+// return to the main simulation loop and continue until we simulated the remaining rocks and we are done
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: go run part1/main.go [file]")
@@ -25,109 +218,120 @@ func main() {
 	file := os.Args[1]
 	content, _ := os.ReadFile(file)
 	split := strings.Split(string(content), "\n")
-	sensors := make([]sensor, 0)
-	var (
-		minx = math.MaxInt
-		maxx int
-	)
+
+	fallingOrder := []shape{minus, plus, l, linePiece, block}
+	jetPattern := make([]string, 0)
 	for _, line := range split {
-		var (
-			sensorx, sensory int
-			beaconx, beacony int
-		)
-		fmt.Sscanf(line, "Sensor at x=%d, y=%d: closest beacon is at x=%d, y=%d", &sensorx, &sensory, &beaconx, &beacony)
-		sensorP := point{x: sensorx, y: sensory}
-		beacon := point{x: beaconx, y: beacony}
-
-		s := sensor{
-			p:        sensorP,
-			coverage: distance(sensorP.x, beacon.x, sensorP.y, beacon.y),
-			beacon:   beacon,
-		}
-		sensors = append(sensors, s)
-
-		if s.p.x-s.coverage < minx {
-			minx = s.p.x - s.coverage
-		}
-		if s.p.x+s.coverage > maxx {
-			maxx = s.p.x + s.coverage
-		}
+		jetPattern = strings.Split(line, "")
 	}
 
-	// Walk through the perimeter of each beacon's area.
-	// For each sensor, construct an area of points that you check.
-	// Add those points into a list of points. ( skip if below 0 or above limit )
-	// For each of those points then check if they are within the
-	// distance of other sensors.
+	y := 0
+	rocks := 1000000000000
+	fallen := 0
+	playground := make(map[point]bool)
+	leftSide := 0
+	rightSide := 7
+	jetPush := 0
+	for fallen != rocks {
+		currentRock := fallingOrder[fallen%len(fallingOrder)]
+		startingY := y + 3
+		current := point{x: 2 - currentRock.mostLeft.x, y: startingY} // This is the location of the designated zeroth coordinate of the shape.
+		canMove := true
+		for canMove {
+			canMove = false
 
-	// It's a map to avoid duplicates from overlapping sensors.
-	limit := 4000000
-	// limit := 20
-	possiblePoints := make(map[point]bool)
-	for _, sensor := range sensors {
-		leftCorner := point{x: sensor.p.x - sensor.coverage - 1, y: sensor.p.y}
-		rightCorner := point{x: sensor.p.x + sensor.coverage + 1, y: sensor.p.y}
-		upperCorner := point{x: sensor.p.x, y: sensor.p.y + sensor.coverage + 1}
-		lowerCorner := point{x: sensor.p.x, y: sensor.p.y - sensor.coverage - 1}
+			currentJet := jetPattern[jetPush%len(jetPattern)]
+			if currentJet == "<" {
+				if current.x-1+currentRock.mostLeft.x >= leftSide && !isSomethingToTheLeft(current, currentRock, playground) {
+					current.x--
+				}
+			} else if currentJet == ">" {
+				if current.x+1+currentRock.mostRight.x < rightSide && !isSomethingToTheRight(current, currentRock, playground) {
+					current.x++
+				}
+			}
+			jetPush++
 
-		start := leftCorner
-		possiblePoints[start] = true
-		for start != lowerCorner {
-			start.x++
-			start.y--
-			possiblePoints[start] = true
-		}
-		start = lowerCorner
-		possiblePoints[lowerCorner] = true
-		for start != rightCorner {
-			start.x++
-			start.y++
-			possiblePoints[start] = true
-		}
-		start = rightCorner
-		possiblePoints[start] = true
-		for start != upperCorner {
-			start.x--
-			start.y++
-			possiblePoints[start] = true
-		}
-		start = upperCorner
-		possiblePoints[start] = true
-		for start != leftCorner {
-			start.x--
-			start.y--
-			possiblePoints[start] = true
-		}
-	}
-
-	fmt.Println("length of possible points: ", len(possiblePoints))
-	for k := range possiblePoints {
-		if k.x < 0 || k.y < 0 || k.x > limit || k.y > limit {
-			continue
-		}
-		inRange := false
-		for _, s := range sensors {
-			distanceToSensor := distance(k.x, s.p.x, k.y, s.p.y)
-			if distanceToSensor <= s.coverage {
-				inRange = true
-				break
+			if (current.y-1+currentRock.bottom.y) >= 0 && !isSomethingDownwards(current, currentRock, playground) {
+				current.y--
+				canMove = true
 			}
 		}
-		if !inRange {
-			fmt.Println("x, y: ", k)
-			fmt.Println("frequency: ", k.x*limit+k.y)
-			os.Exit(0)
+
+		for _, p := range allPoints(current, currentRock) {
+			playground[p] = true
+		}
+		if current.y+currentRock.height > y {
+			y = current.y + currentRock.height
+		}
+
+		fallen++
+	}
+
+	fmt.Println("The tower is this tall: ", y)
+}
+
+func display(playground map[point]bool, maxy int, current point, rock shape) {
+	contains := func(p point, current point, list []point) bool {
+		for _, v := range list {
+			if p.x == v.x+current.x && p.y == v.y+current.y {
+				return true
+			}
+		}
+
+		return false
+	}
+	for y := maxy + 7; y >= 0; y-- {
+		for x := 0; x < 7; x++ {
+			if playground[point{x: x, y: y}] {
+				fmt.Print("#")
+			} else if contains(point{x: x, y: y}, current, rock.all) {
+				fmt.Print("@")
+			} else {
+				fmt.Print(".")
+			}
+		}
+		fmt.Println()
+	}
+	fmt.Println("-------")
+}
+
+func allPoints(current point, rock shape) []point {
+	var calculated []point
+	for _, p := range rock.all {
+		calculated = append(calculated, point{x: current.x + p.x, y: current.y + p.y})
+	}
+
+	return calculated
+}
+
+func isSomethingToTheLeft(current point, currentRock shape, playground map[point]bool) bool {
+	for _, p := range currentRock.leftSide {
+		// -1 because we are checking the neighbour
+		if playground[point{x: p.x + current.x - 1, y: current.y + p.y}] {
+			return true
 		}
 	}
+	return false
 }
 
-func distance(x1, x2, y1, y2 int) int {
-	return abs(x1-x2) + abs(y1-y2)
-}
-
-func abs(x int) int {
-	if x < 0 {
-		return -x
+func isSomethingToTheRight(current point, currentRock shape, playground map[point]bool) bool {
+	// +1 because we are checking the neighbour
+	for _, p := range currentRock.rightSide {
+		if playground[point{x: p.x + current.x + 1, y: current.y + p.y}] {
+			return true
+		}
 	}
-	return x
+	return false
+}
+
+// include the bottom line which is y == 0.
+func isSomethingDownwards(current point, currentRock shape, playground map[point]bool) bool {
+	// -1 because we are checking the neighbour
+	for _, p := range currentRock.bottomSide {
+		if playground[point{x: p.x + current.x, y: current.y + p.y - 1}] {
+			return true
+		}
+	}
+	return false
 }
